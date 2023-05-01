5e18fae7000a3a0b841a58a14f2710520943b8f3
victorgubskih <112268920+victorgubskih@users.noreply.github.com>
2023-05-01 22:12:13 +0300

    creating project

---
+++ WeatherMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMap
+//
+//  Created by viktor on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
